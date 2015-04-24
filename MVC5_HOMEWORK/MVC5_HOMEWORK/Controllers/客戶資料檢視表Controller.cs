26b07b76259060c4a24b8a2e23d681e5e32097ff
Allen <sendohlun@gmail.com>
2015-04-24 15:02:12 +0800

    建立客戶資料報表

---
+++ MVC5_HOMEWORK/MVC5_HOMEWORK/Controllers/客戶資料檢視表Controller.cs
@@ -0,0 +1,20 @@
+﻿using MVC5_HOMEWORK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_HOMEWORK.Controllers
+{
+    public class 客戶資料檢視表Controller : Controller
+    {
+        // GET: 客戶資料檢視表
+        public ActionResult Index()
+        {
+            客戶資料Entities1 en = new 客戶資料Entities1();
+            IQueryable l_客戶相關資料表 = en.客戶相關資料表.AsQueryable();
+            return View(l_客戶相關資料表);
+        }
+    }
+}
